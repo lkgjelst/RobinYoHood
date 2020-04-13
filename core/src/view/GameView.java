88855554a5be4385998b149bd363a836f2c33003
lkgjelst <lkgjelst@stud.ntnu.no>
2020-04-13 17:55:26 +0200

    added views and controller

---
+++ core/src/view/GameView.java
@@ -0,0 +1,49 @@
+package view;
+
+import com.badlogic.gdx.Screen;
+
+import controller.Controller;
+
+public class GameView extends View {
+
+    private Controller parent;
+
+    public GameView(Controller parent) {
+        this.parent = parent;
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
---
+++ core/src/view/View.java
@@ -0,0 +1,8 @@
+package view;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+
+public abstract class View extends Stage implements Screen {
+
+}
